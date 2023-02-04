74cfae09b810bfa4e3d0bb83167c5ec1cde721d2
Nelli_Ivanova <115640860+nelli04@users.noreply.github.com>
2023-02-04 18:49:06 +0300

    01

---
+++ src/demo.tsx
@@ -0,0 +1,36 @@
+/*type TopCarsType = {
+    manufacturer: string
+    model: string
+}
+type CarsType = {
+    topCars: Array<TopCarsType>
+}
+const topCars = [
+    {manufacturer: 'BMW', model: 'm5cs'},
+    {manufacturer: 'Mercedes', model: 'e63s'},
+    {manufacturer: 'Audi', model: 'rs6'}
+]
+export const Demo = (props: CarsType) => {
+    // @ts-ignore
+    return (
+        <table>
+            {props.topCars.map((cars, index) =>
+                <th key={index}>
+                    {cars.model}
+                    {cars.manufacturer}
+                </th>
+            )}
+
+        </table>
+    )
+}*/
+
+export function Demo (){
+
+return (
+        <div>
+            <button>ClickMe</button>
+        </div>
+    )
+
+}
