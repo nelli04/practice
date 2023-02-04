41ac6dbc4ef085829a94648c33cb7102a280ab41
Nelli_Ivanova <115640860+nelli04@users.noreply.github.com>
2023-02-04 18:46:26 +0300

    01

---
+++ src/TASKS.tsx
@@ -0,0 +1,20 @@
+type DataType = {
+    title: string
+    tasks: Array<TasksType>
+    students: Array<string>
+}
+type TasksType = {
+    taskId: number
+    title: string
+    isDone: boolean
+}
+export const TASKS = (props: DataType) => {
+    return (
+        <ul>
+            <li>
+                {props.students.map((DataType) => {return (DataType)})}
+            </li>
+        </ul>
+    )
+}
+
