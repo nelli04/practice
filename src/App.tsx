79d4313b237761ee99c5c068bd15bf8ad588bba7
Nelli_Ivanova <115640860+nelli04@users.noreply.github.com>
2023-02-04 22:53:47 +0300

    01

--- src/App.tsx
+++ src/App.tsx
@@ -1,195 +1,59 @@
 import React, {useState} from 'react';
 import './App.css';
-import {TASKS} from "./Demo/TASKS";
-import {Demo} from "./Demo/demo";
-import {Button} from "./Demo/Button";
-
-
-
 
 // Hi Guys!
 // Let's reinforce our current session!
 // -You have 2 arrays. You should create a new component TASKS, where you will render these arrays.
 // -Don't forget to assign types to our data.
 
-const data1 = {
-    title: "What to do",
-    tasks: [
-        {taskId: 1, title: "HTML&CSS2", isDone: true},
-        {taskId: 2, title: "JS2", isDone: true}
-    ],
-    students: [
-        'Jago Wormald1',
-        'Saul Milne2',
-        'Aariz Hester3',
-        'Dion Reeve4',
-        'Anisa Ortega5',
-        'Blade Cisneros6',
-        'Malaikah Phelps7',
-        'Zeeshan Gallagher8',
-        'Isobella Vo9',
-        'Rizwan Mathis10',
-        'Menaal Leach11',
-        'Kian Walton12',
-        'Orion Lamb13',
-        'Faizah Huynh14',
-        'Crystal Vaughan15',
-        'Vivien Hickman16',
-        'Stuart Lu17',
-        'Karol Davison18',
-        'Dario Burns19',
-        'Chloe Rich20',
-        'Martyna Felix',
-        'Nida Glass',
-        'Maeve Miles',
-        'Hasnain Puckett',
-        'Ayman Cano',
-        'Safwan Perry',
-        'Fox Kelly',
-        'Louise Barlow',
-        'Malaki Mcgill',
-        'Leanna Cline',
-        'Willard Hodge',
-        'Amelia Dorsey',
-        'Kiah Porter',
-        'Jeanne Daly',
-        'Mohsin Armstrong',
-        'Laurie Rangel',
-        'Princess Tierney',
-        'Kasim Kendall',
-        'Darryl Cope',
-        'Elysha Ray',
-        'Liyana Harris',
-        'Kashif Blackburn',
-        'Atif Zimmerman',
-        'Sila Hartley',
-        'Ralphie Hebert',
-    ]
-}
-const data2 =   {
-    title: "What to learn",
-    tasks: [
-        {taskId: 1, title: "HTML&CSS", isDone: true},
-        {taskId: 2, title: "JS", isDone: true}
-    ],
-    students: [
-        'Rick Kane',
-        'Finnlay Bentley',
-        'Samia North',
-        'Isaac Morton',
-        'Lily-Ann Clifford',
-        'Thalia Park',
-        'Sapphire Cruz',
-        'Cieran Vazquez',
-        'Anya Estes',
-        'Dominika Field',
-        'Rosanna Chung',
-        'Safiyah Davey',
-        'Ryley Beasley',
-        'Kalvin Trejo',
-        'Evie-Mae Farrell',
-        'Juliet Valencia',
-        'Astrid Austin',
-        'Lyle Montgomery',
-        'Nisha Mora',
-        'Kylie Callaghan',
-        'Star Wilks',
-        'Marissa Colley',
-        'Asa Fuller',
-        'Leigh Kemp',
-        'Avleen Dawson',
-        'Sammy Bonilla',
-        'Acacia Becker',
-        'Coral Shepherd',
-        'Melina Molina',
-        'Kiran Bailey',
-        'Clara Escobar',
-        'Alexandru Horn',
-        'Brandon-Lee Mercado',
-        'Elouise Weston',
-        'King Long',
-        'Kerri Searle',
-        'Kanye Hamer',
-        'Elwood Benitez',
-        'Mikail Whitaker',
-        'Bobby Hardy',
-        'Talha Ferry',
-        'Priscilla Landry',
-        'Olivia-Grace Cain',
-        'Kiaan Wallace',
-        'Wesley Padilla90',
-        'Ella-Grace Wooten91',
-        'Kaif Molloy92',
-        'Kamal Broadhurst93',
-        'Bianca Ferrell94',
-        'Micheal Talbot95',
-    ]
-}
-const topCars = [
-    {manufacturer: 'BMW', model: 'm5cs'},
-    {manufacturer: 'Mercedes', model: 'e63s'},
-    {manufacturer: 'Audi', model: 'rs6'}
-]
-function App() {
-    const Button1 = (sub: string, age: number) => {
-        console.log(sub, age)
-    }
-    const Button2 = (sub: string, age: number) => {
-        console.log(sub, age)
-    }
-    const Button3 = () => {
-        console.log('Im stuped buttom')
-    }
-    //let a = 1;
-    let[a, setA] = useState(1)
-    const onClicks = () => {
-        setA(++a)
-        console.log(a)
-    }
-    const onClicks1 = () => {
-        setA(--a)
-        console.log(a)
-    }
+type Filter = 'all' | 'RUBLS' | 'Dollars'
 
+function App() {
+    const [money, setMoney] = useState([
+        {banknots: 'Dollars', value: 100, number: ' a1234567890'},
+        {banknots: 'Dollars', value: 50, number: ' z1234567890'},
+        {banknots: 'RUBLS', value: 100, number: ' w1234567890'},
+        {banknots: 'Dollars', value: 100, number: ' e1234567890'},
+        {banknots: 'Dollars', value: 50, number: ' c1234567890'},
+        {banknots: 'RUBLS', value: 100, number: ' r1234567890'},
+        {banknots: 'Dollars', value: 50, number: ' x1234567890'},
+        {banknots: 'RUBLS', value: 50, number: ' v1234567890'},
+    ])
+    let [fil, setFil] = useState<Filter>('all')
+    let filtered = money
+    if (fil === 'RUBLS') {
+        filtered = money.filter(mFilter => mFilter.banknots === 'RUBLS')
+    }
+    if (fil === 'Dollars') {
+        filtered = money.filter(mFilter => mFilter.banknots === 'Dollars')
+    }
+    const handler = (nameBut: Filter) => {
+        setFil(nameBut)
+    }
     return (
-        <div className="App">
-            <h1>{a}</h1>
-            <button onClick={onClicks}>number</button>
-            <button onClick={onClicks1}>0</button>
-            {/*<Button name={'ClickME'} callBack={() => Button1('Hi', 19)}/>
-            <Button name={'ClickME2'} callBack={() =>Button2('Hello',32)}/>
-            <Button name={'Click'} callBack={Button3}/>*/}
-           {/* <Demo/>*/}
-           {/* <TASKS title={data1.title} tasks={data1.tasks} students={data1.students} />
-            <TASKS title={data2.title} tasks={data2.tasks} students={data2.students} />*/}
-        </div>
+        <>
+            <ul>
+                {filtered.map((m, index) => {
+                    return (
+
+                        <li key={index}>
+                            <span>{m.banknots}</span>
+                            <span>{m.value}</span>
+                            <span>{m.number}</span>
+                        </li>
+                    )
+                })}
+            </ul>
+            <div style={{marginLeft: '35px'}}>
+                <button onClick={() => handler('all')}>all</button>
+                <button onClick={() => handler('RUBLS')}>rubles</button>
+                <button onClick={() => handler('Dollars')}>dollars</button>
+            </div>
+        </>
     );
 }
 
 export default App;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 //--------------------------------------------------------------
