0d8201dd2d5dfd1a42420c0b9a2cef548697af41
Nelli_Ivanova <115640860+nelli04@users.noreply.github.com>
2023-02-10 17:40:03 +0300

    input

---
+++ src/FullInput.tsx
@@ -0,0 +1,23 @@
+import React, {ChangeEvent, useState} from 'react';
+
+type FullInputType = {
+    addMessage: (title: string) => void
+}
+
+export function FullInput (props: FullInputType) {
+    let[title, setTitle] = useState('')
+    console.log(title)
+    const onChangeInputHandler = (event: ChangeEvent<HTMLInputElement>) => {
+        setTitle(event.currentTarget.value)
+    }
+    const onClickButtonHandler = () => {
+        props.addMessage(title)
+        setTitle('')
+    }
+    return (
+        <div>
+                <input value={title} onChange={onChangeInputHandler}/>
+                <button onClick={onClickButtonHandler}>+</button>
+        </div>
+    )
+}
