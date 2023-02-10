fbefb7e0c5cc17293b29b3979de2cb348cffb13d
Nelli_Ivanova <115640860+nelli04@users.noreply.github.com>
2023-02-10 17:39:15 +0300

    input

---
+++ src/Button.tsx
@@ -0,0 +1,21 @@
+import React, {useState} from 'react';
+
+type ButtonPropsType = {
+    name: string
+    callBack: () => void
+}
+
+
+
+
+const Button = (props: ButtonPropsType) => {
+    const onClickButtonHandler = () => {
+        props.callBack()
+    }
+
+    return (
+       <button onClick={onClickButtonHandler}>{props.name}</button>
+    );
+};
+
+export default Button;
