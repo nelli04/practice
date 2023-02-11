8bfa506203e0fe37b326ed6af83214d9be87d51e
Nelli_Ivanova <115640860+nelli04@users.noreply.github.com>
2023-02-11 22:19:47 +0300

    input

--- src/MONDAY/NativeJS/Dimych/React/Rating.tsx
+++ src/MONDAY/NativeJS/Dimych/React/Rating.tsx
@@ -15,64 +15,6 @@ export function Rating(props: RatingType) {
             <Star selected={props.value > 5}/>
         </div>
     )
-
-    /*if (props.value === 1) {
-        return (
-
-    }
-    if (props.value === 2) {
-        return (
-            <div>
-                <Star selected={true}/>
-                <Star selected={true}/>
-                <Star selected={false}/>
-                <Star selected={false}/>
-                <Star selected={false}/>
-            </div>)
-    }
-    if (props.value === 3) {
-        return (
-            <div>
-                <Star selected={true}/>
-                <Star selected={true}/>
-                <Star selected={true}/>
-                <Star selected={false}/>
-                <Star selected={false}/>
-            </div>)
-    }
-    if (props.value === 4) {
-        return (
-            <div>
-                <Star selected={true}/>
-                <Star selected={true}/>
-                <Star selected={true}/>
-                <Star selected={true}/>
-                <Star selected={false}/>
-            </div>)
-    }
-    if (props.value === 5) {
-        return (
-            <div>
-                <Star selected={true}/>
-                <Star selected={true}/>
-                <Star selected={true}/>
-                <Star selected={true}/>
-                <Star selected={true}/>
-            </div>)
-    }
-    return (
-        <div>
-             {
-            <div>
-            <Star selected={false}/>
-            <Star selected={false}/>
-            <Star selected={false}/>
-            <Star selected={false}/>
-            <Star selected={false}/>
-            </div>
-        }
-        </div>
-    )*/
 }
 
 type StarType = {
