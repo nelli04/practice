2d01c65277b2be8f6665e6ca402b3995165dfbc3
Nelli_Ivanova <115640860+nelli04@users.noreply.github.com>
2023-02-11 22:14:31 +0300

    input

--- src/App.tsx
+++ src/App.tsx
@@ -9,26 +9,6 @@ import Button from "./Button";
 
 /*export type FilterValuesType = "all" | "active" | "completed" | "delete" | "treeTask";*/
 
-//Hi guys!
-//1. Let's create a 'DELETE ALL TASKS' button, and place it above the filter buttons
-//Clicking the button removes all tasks
-//2. Let's create a fourth filter button-if you click it, the first three tasks will be displayed
-//3. Relocate everything associated with  filters to the Todolist.tsx component. Make it work
-//
-// let [filter, setFilter] = useState<FilterValuesType>("all");
-//
-// let tasksForTodolist = tasks;
-//
-// if (filter === "active") {
-//     tasksForTodolist = tasks.filter(t => t.isDone === false);
-// }
-// if (filter === "completed") {
-//     tasksForTodolist = tasks.filter(t => t.isDone === true);
-// }
-//
-// function changeFilter(value: FilterValuesType) {
-//     setFilter(value);
-// }
 
 function App() {
     const addMessage = (title: string) => {
