a8e823667dbc972e24a1755ea8d8d8b7330ea915
Nelli_Ivanova <115640860+nelli04@users.noreply.github.com>
2023-02-11 21:55:44 +0300

    input

--- src/App.tsx
+++ src/App.tsx
@@ -87,32 +87,4 @@ function App() {
 export default App;
 
 
-/* let [tasks, setTasks] = useState([
-        {id: 1, title: "HTML&CSS", isDone: true},
-        {id: 2, title: "JS", isDone: true},
-        {id: 3, title: "ReactJS", isDone: false},
-        {id: 4, title: "Rest API", isDone: false},
-        {id: 5, title: "GraphQL", isDone: false},
-    ]);
-    function removeTask(id: number) {
-        let filteredTasks = tasks.filter(t => t.id != id);
-        setTasks(filteredTasks);
-    }
-    return (
-        <div className="App">
-            <Todolist title="What to learn"
-                      tasks={tasks}
-                      removeTask={removeTask}/>
-        </div>
-    );*/
-
-//-------------------------------------------------------------------------
-
-// import React, {useState} from 'react';
-// import './App.css';
-// import {Todolist} from './Todolist';
-//
-//
-// export type FilterValuesType = "all" | "active" | "completed" | "three";
-
 //걸어본다 - B1A4; 내꺼하자 - INFINITE
