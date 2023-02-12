c4eeb50d4478342fc679bfea4858e65c27e9c3c3
Nelli_Ivanova <115640860+nelli04@users.noreply.github.com>
2023-02-12 21:29:40 +0300

    input

--- src/App.tsx
+++ src/App.tsx
@@ -40,4 +40,66 @@ function App() {
 export default App;
 
 
-//걸어본다 - B1A4; 내꺼하자 - INFINITE
+//걸어본다 - B1A4; 내꺼하자 - INFINITE; 외톨이야 - CNBLUE
+
+/*
+let students = [
+    {
+        name: "Bob",
+        age: 22,
+        isMarried: true,
+        scores: 85,
+    },
+    {
+        name: "Alex",
+        age: 21,
+        isMarried: true,
+        scores: 89
+    },
+    {
+        name: "Nick",
+        age: 20,
+        isMarried: false,
+        scores: 120
+    },
+    {
+        name: "John",
+        age: 19,
+        isMarried: false,
+        scores: 100
+    }
+];
+
+const getNames = (array) => {
+    const result = new Array();
+    const getName = obj => obj.name
+    for (let i = 0; i < array.length; i++) {
+        const temp =  getName(array[i])
+        result[i] = temp
+    }
+
+    return result
+}
+const name = getNames(students)
+console.log(getNames(students));
+
+const addCScore = (array) => {
+    const result = new Array();
+    const addScore = (obj) => {
+        const copy = {...obj}
+        copy.scores = obj.scores + 10
+        return copy
+    }
+    return result;
+}
+
+const selfMadeMap = (array, func) => {
+    const result = []
+    const addScore = (obj) => {
+        const copy = {...obj}
+        copy.scores = obj.scores + 10
+        return copy
+    }
+    return result;
+*/
+//}
