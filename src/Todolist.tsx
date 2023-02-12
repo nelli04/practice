e3d7af590f651ce8be1c180c83d3378da8198870
Nelli_Ivanova <115640860+nelli04@users.noreply.github.com>
2023-02-12 21:48:46 +0300

    -

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,4 +1,17 @@
+function sumTwoSmallestNumbers(numbers) {
+    // numbers = numbers.sort((a, b) => a - b)
+    for (let j = 0; j < numbers.length; j++) {
+        for (let i = 0; i < numbers.length; i++) {
+            if(numbers[i] > numbers[i + 1]){
+                let temp = numbers[i]
+                numbers[i] = numbers[i + 1]
+                numbers[i + 1] = temp
+            }
+        }
+    }
 
+    return numbers[0] + numbers[1]
+}
 
 
 
