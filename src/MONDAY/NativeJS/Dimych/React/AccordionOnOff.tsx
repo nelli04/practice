b90411e95c7dbd567ce0e81754e7717fd645c2c8
Nelli_Ivanova <115640860+nelli04@users.noreply.github.com>
2023-02-16 19:53:09 +0300

    03

---
+++ src/MONDAY/NativeJS/Dimych/React/AccordionOnOff.tsx
@@ -0,0 +1,45 @@
+import React, {ChangeEvent, MouseEventHandler, useState} from "react";
+
+type AccordionType = {
+    //collapsed: boolean
+    titleV: string
+}
+
+type AccordionPropsType = {
+    title: string
+}
+
+export function AccordionOnOff(props: AccordionType) {
+
+    let [collapsed, setCollapsed] = useState(true)
+    /*if (collapsed === true) {
+        return () => setCollapsed(true)
+    } else {
+        return () => setCollapsed(false)
+    }
+*/
+        return (
+            <div>
+                {<AccordionTitle title={props.titleV} />} <button onClick={() => setCollapsed( collapsed ? false : true)}>TOGGLE</button>
+                {collapsed && <AccordionBody/>}
+            </div>
+        )
+}
+
+function AccordionTitle(props: AccordionPropsType) {
+    return (
+        <h3>{props.title}</h3>
+    )
+}
+
+function AccordionBody() {
+    return (
+        <div>
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+            </ul>
+        </div>
+    )
+}
