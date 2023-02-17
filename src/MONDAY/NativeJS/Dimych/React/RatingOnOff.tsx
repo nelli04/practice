b6b4913d06c2931f11a5e4bc31dd458570248ebe
Nelli_Ivanova <115640860+nelli04@users.noreply.github.com>
2023-02-17 12:01:18 +0300

    03

--- src/MONDAY/NativeJS/Dimych/React/RatingOnOff.tsx
+++ src/MONDAY/NativeJS/Dimych/React/RatingOnOff.tsx
@@ -6,30 +6,54 @@ type RatingType = {
 
 export function RatingOnOff(props: RatingType) {
 
-    let [selectedd, setSelected] = useState(true)
+    let [selectedd, setSelected] = useState(0)
 
     return (
         <div>
-            <Star selected={true}/><button onClick={ () => {setSelected(false)} }>1</button>
-            <Star selected={true}/><button onClick={ () => {setSelected(selectedd)} }>2</button>
-            <Star selected={true}/><button onClick={ () => {setSelected(selectedd)} }>3</button>
-            <Star selected={true}/><button onClick={ () => {setSelected(selectedd)} }>4</button>
-            <Star selected={true}/><button onClick={ () => {setSelected(selectedd)} }>5</button>
-            <Star selected={true}/><button onClick={ () => {setSelected(selectedd)} }>6</button>
+            <Star selectedd={selectedd > 0}/>
+            <button onClick={() => {
+                setSelected(1)
+            }}>1
+            </button>
+            <Star selectedd={selectedd > 1}/>
+            <button onClick={() => {
+                setSelected(2)
+            }}>2
+            </button>
+            <Star selectedd={selectedd > 2}/>
+            <button onClick={() => {
+                setSelected(3)
+            }}>3
+            </button>
+            <Star selectedd={selectedd > 3}/>
+            <button onClick={() => {
+                setSelected(4)
+            }}>4
+            </button>
+            <Star selectedd={selectedd > 4}/>
+            <button onClick={() => {
+                setSelected(5)
+            }}>5
+            </button>
         </div>
     )
 }
 
 type StarType = {
-    selected: boolean
+    selectedd: boolean
 }
 
 function Star(props: StarType) {
-    if (props.selected === true) {
+
+    return (
+        <div>
+            {props.selectedd ? <span><b> star </b></span> : <span> star </span>}
+        </div>
+    )
+}
+
+/* if (props.selectedd == true) {
         return <span><b> star </b></span>
     } else {
         return <span> star </span>
-    }
-
-}
-
+    }*/
