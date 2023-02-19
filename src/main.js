3e36a5730e28751e843012f64fa6fec1cf81c8ab
Nelli_Ivanova <115640860+nelli04@users.noreply.github.com>
2023-02-19 21:31:14 +0300

    03

---
+++ src/main.js
@@ -0,0 +1,28 @@
+const sm = document.getElementById('small')
+const md = document.getElementById('medium')
+const bg = document.getElementById('big')
+
+const onClickHandler1 = (e) => {
+    alert('small')
+}
+const onClickHandler2 = (e) => {
+   alert('medium')
+}
+const onClickHandler3 = (e) => {
+    alert('big')
+    console.log(e.currentTarget)
+}
+//onClickHandler({...})
+
+sm.onclick = onClickHandler1
+sm.onclick = null
+
+sm.addEventListener('click', onClickHandler1)
+md.addEventListener('click', onClickHandler2)
+bg.addEventListener('click', onClickHandler3)
+// addEventListener с помощью него можно нацепить несколько обработчиков событий
+// addEventListener позволяет рассматривать как погружение и всплытие
+//this ссылка на тот объект который вызываем
+// делегирование событий
+// Дефолтное поведение form and href
+// семантическая верстка
