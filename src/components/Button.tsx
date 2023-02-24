7463a0ce77627da04748089c2c96e6f0113d3d27
Nelli_Ivanova <115640860+nelli04@users.noreply.github.com>
2023-02-24 22:05:57 +0300

    04

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,8 +1,6 @@
 import React, {useState} from 'react';
 
 
-
-
 type BossType = {
     buttonInc: () => void
     buttonReset: () => void
@@ -14,10 +12,9 @@ export const Button = (p: BossType) => {
 
     return (
         <div>
-
             <div className='btn'>
-                <button className='inc' onClick={p.buttonInc}>inc</button>
-                <button className={p.click >= 1 ? 'reset' : 'true_false'} onClick={p.buttonReset}>reset</button>
+                <button className='inc' /*onClick={p.buttonInc}*/>inc</button>
+                <button className={p.click >= 1 ? 'reset' : 'true_false'} /*onClick={p.buttonReset}*/>reset</button>
             </div>
         </div>
     );
