9d03b706f0bae0cb0e8f8bba1199bab5fadd9c02
Nelli_Ivanova <115640860+nelli04@users.noreply.github.com>
2023-02-24 20:39:56 +0300

    04

---
+++ src/components/Button.tsx
@@ -0,0 +1,24 @@
+import React, {useState} from 'react';
+
+
+
+
+type BossType = {
+    buttonInc: () => void
+    buttonReset: () => void
+    click: number
+}
+
+export const Button = (p: BossType) => {
+
+    return (
+        <div>
+
+            <div className='btn'>
+                <button className='inc' onClick={p.buttonInc}>inc</button>
+                <button className={p.click >= 1 ? 'reset' : 'true_false'} onClick={p.buttonReset}>reset</button>
+            </div>
+        </div>
+    );
+};
+
