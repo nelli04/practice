27006ffa10cfc339a8729cee4969fdfebf069a14
Nelli_Ivanova <115640860+nelli04@users.noreply.github.com>
2023-02-25 12:31:39 +0300

    04

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -12,8 +12,8 @@ export const Button = (p: BossType) => {
     return (
         <div>
             <div className='btn'>
-                <button className='inc' /*onClick={p.buttonInc}*/>inc</button>
-                <button className={p.click >= 1 ? 'reset' : 'true_false'} /*onClick={p.buttonReset}*/>reset</button>
+                <button className='inc' onClick={p.buttonInc}>inc</button>
+                <button className={p.click >= 1 ? 'reset' : 'true_false'} onClick={p.buttonReset}>reset</button>
             </div>
         </div>
     );
