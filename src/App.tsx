a1eff1ce7d62f25961aece53589cecde191ef529
Nelli_Ivanova <115640860+nelli04@users.noreply.github.com>
2023-02-27 12:46:55 +0300

    04

--- src/App.tsx
+++ src/App.tsx
@@ -1,80 +1,18 @@
 import React, {useState} from 'react';
 import './App.css';
-import {Todolist} from './Todolist';
-import {v1} from 'uuid';
+import {Accordion} from "./MONDAY/NativeJS/Dimych/React/Accordion";
 
-
-// Hi guys!
-//1.Let`s wrap up our bunch  of filters in a function.
-//How can we do this? We can wrap all our goodies into a new function:
-//     const filteredTasks=()=>{
-//          let tasksForTodolist = tasks;
-//
-//          if (filter === "active") {
-//          return   tasksForTodolist = tasks.filter(t => !t.isDone);
-//          }
-//          if (filter === "completed") {
-//            return  tasksForTodolist = tasks.filter(t => t.isDone);
-//          }
-//          return tasksForTodolist
-//     }
-
-//2.Let`s replace our if`s with  switch.
-
-
-export type FilterValuesType = "all" | "active" | "completed";
 
 function App() {
 
-    let [tasks, setTasks] = useState([
-        {id: v1(), title: "HTML&CSS", isDone: true},
-        {id: v1(), title: "JS", isDone: true},
-        {id: v1(), title: "ReactJS", isDone: false},
-        {id: v1(), title: "Rest API", isDone: false},
-        {id: v1(), title: "GraphQL", isDone: false},
-    ]);
-
-    function removeTask(id: string) {
-        let filteredTasks = tasks.filter(t => t.id != id);
-        setTasks(filteredTasks);
-    }
-
-    function addTask(title: string) {
-        let task = {id: v1(), title: title, isDone: false};
-        let newTasks = [task, ...tasks];
-        setTasks(newTasks);
-    }
-
-    let [filter, setFilter] = useState<FilterValuesType>("all");
-
-    const filteredTasks = () => {
-        let tasksForTodolist = tasks;
-
-        switch (filter) {
-            case "active":
-                return tasksForTodolist = tasks.filter(t => !t.isDone);
-        }
-        switch (filter) {
-            case "completed":
-                return tasksForTodolist = tasks.filter(t => t.isDone);
-        }
-        return tasksForTodolist
-    }
-
-    function changeFilter(value: FilterValuesType) {
-        setFilter(value);
-    }
-
+    let [acc, setAcc] = useState(false)
 
     return (
-        <div className="App">
-            <Todolist title="What to learn"
-                      tasks={filteredTasks()}
-                      removeTask={removeTask}
-                      changeFilter={changeFilter}
-                      addTask={addTask}/>
-        </div>
-    );
+     <div>
+         <Accordion collapsed={acc} titleV={'Menu'} on={() => {setAcc(!acc)}}/>
+     </div>
+    )
+
 }
 
 export default App;
